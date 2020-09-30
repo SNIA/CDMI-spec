13d71f1b403437851d2193fad027f64cd979c57b
David Slik <dslik@acm.org>
2020-09-30 15:41:15 -0700

    Added exclude to ignore extensions when building spec

--- conf.py
+++ conf.py
@@ -75,7 +75,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = [ 'README.md', 'license.md', '**/scratch.txt' ]
+exclude_patterns = [ 'README.md', 'license.md', '**/scratch.txt', 'cdmi_extensions' ]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
